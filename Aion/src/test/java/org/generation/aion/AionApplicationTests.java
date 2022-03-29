962d6b5edde9e5469e040bb4adc89c6d5578eb6c
Jokyeon <jhonathan.fs@hotmail.com>
2022-03-29 12:02:34 -0300

    Inicialização do projeto

---
+++ Aion/src/test/java/org/generation/aion/AionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.aion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
