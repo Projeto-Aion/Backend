c018ffa9558ef8dc20cef2d3a9e4403b3e7a8c97
rafaelmoreira-lima <rafaelmoreira.lima@hotmail.com>
2022-04-08 17:27:59 -0300

    model tema

--- Aion/src/main/java/org/generation/aion/model/postagem.java
+++ Aion/src/main/java/org/generation/aion/model/postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_postagens")
@@ -36,6 +39,11 @@ public class postagem {
 	@Size(max=280)
 	private String descricao;
 
+	@ManyToOne
+	@JsonIgnoreProperties
+	
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
