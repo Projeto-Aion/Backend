b76655e2310a9489aef11a5c58486a38d4987f8c
Jokyeon <jhonathan.fs@hotmail.com>
2022-05-12 15:38:12 -0300

    correção TokenGenerator

--- src/main/java/org/generation/aion/service/UsuarioService.java
+++ src/main/java/org/generation/aion/service/UsuarioService.java
@@ -58,15 +58,13 @@ public class UsuarioService {
 
 
         if (usuario.isPresent()) {
-            if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+            if (compararSenhas(usuarioLogin.get().getSenha(),usuario.get().getSenha())) {
                 usuarioLogin.get().setId(usuario.get().getId());
                 usuarioLogin.get().setNome(usuario.get().getNome());
                 usuarioLogin.get().setFoto(usuario.get().getFoto());
-                usuarioLogin.get().setSenha(usuario.get().getSenha());
                 usuarioLogin.get().setTipo(usuario.get().getTipo());
-                usuarioLogin.get().setToken(geradorBasicToken(
-                usuarioLogin.get().getUsuario(),
-                usuarioLogin.get().getSenha()));
+                usuarioLogin.get().setToken(geradorBasicToken(usuarioLogin.get().getUsuario(),usuarioLogin.get().getSenha()));
+                usuarioLogin.get().setSenha(usuario.get().getSenha());
 
                 return usuarioLogin;
             }
