4466c1266afde0c7c339434f062167d78a27dcf9
Nathalia <99620798+iamnathalia@users.noreply.github.com>
2022-05-18 12:05:46 -0300

    retirada de notnull em categoria

--- src/main/java/org/generation/aion/model/Tema.java
+++ src/main/java/org/generation/aion/model/Tema.java
@@ -21,7 +21,7 @@ public class Tema {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	
 	private String categoria;
 	
 	@OneToMany(mappedBy="tema", cascade=CascadeType.ALL)
